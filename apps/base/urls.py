fc477cb7942a25cb53188dbdd902705a94ee1ddb
nurai003002 <nuraj9663@gmail.com>
2024-01-11 16:03:36 +0600

    update3

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('faq/', views.faq, name='faq'),
     path('service_detail/<int:id>/', views.service_detail, name='service_detail'),
     path("service/", views.service, name="service"),
+    path('price', views.price, name='price'),
+    path('team_details/<int:id>/', views.team_details, name='team_details')
 
 ]
